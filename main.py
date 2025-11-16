4fe8334118163fb5d58877069ebbdcf0b98531e0
Jeremy Ellison <jkellison980@gmail.com>
2025-11-15 23:48:57 -0600

    successful test of sorted search function

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import reference_functions
 
-
-print(f'Hello World.')
-x = 5
-x = 6
-y = 5
-if x == 5:
-    y = 8
-else:
-    y = 9
-
-
+# Test binary_search_sorted_array function
+inputList = [2, 5, 9, 11, 14, 18, 21, 24, 27, 30, 34, 38, 41, 45, 48, 52, 57, 61, 66, 70]
+target = 21
+location = reference_functions.binary_search_sorted_array(inputList,target)
+print(f'\n\nlocation of number: {location}\n\n')
--- reference_functions.py
+++ reference_functions.py
@@ -1,1 +1,33 @@
-def binary_sort (list list1):
+#def binary_sort (arr):
+
+
+def binary_search_sorted_array (arr, findMe):
+    """
+    Perform a Binary search of a sorted array of values to find if a single value is in the array.
+
+    Args:
+        arr -> input array of sorted values to be searched
+        FindMe -> target value to be found
+
+    Returns:
+        The index of the target value if found. Otherwise -1.
+
+    Notes to know:
+        Time Comlpexity - O(logn)
+        Space Compexity - O(1) (this can vary based on other search methods used)
+    """
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        middle = (left + right) // 2
+
+        if arr[middle] == findMe:
+            return middle
+        
+        if arr[middle] < findMe:
+            left = middle + 1
+        else:
+            right = middle - 1
+
+    return -1
