8986678afa50ed047478df180005c33dae9b40e4
Jeremy Ellison <jkellison980@gmail.com>
2025-11-18 13:59:10 -0600

    moving the race classes to the right file cause I am an idiot

--- src/races.py
+++ src/races.py
@@ -1,1 +1,66 @@
-#create classes that have each race and its attributes listed and able to be applied to the character attached to it
+'''
+Race class to hold and organize abilities and such for different races
+
+Jeremy Ellison
+Nov. 18, 2025
+'''
+
+class Race:
+    def apply_stat_bonus(self, character):
+        raise NotImplementedError("The individual race must impliment this function")
+
+
+'''
+Human Race
+Stats
+    STR +1
+    DEX +1
+    WIS +1
+
+Abilities
+    TBD
+
+Feats
+    TBD
+'''
+class Human(Race):
+    def apply_stat_bonus(self, character):
+        character.str_score += 1
+        character.dex_score += 1
+        character.wis_score += 1
+
+
+'''
+Elf Race
+Stats
+    DEX +2
+    INT +1
+
+Abilities
+    TBD
+
+Feats
+    TBD
+'''
+class Elf(Race):
+    def apply_stat_bonus(self, character):
+        character.dex_score += 2
+        character.int_score += 1
+
+
+'''
+Dwarf Race
+Stats
+    STR +1
+    CON +2
+
+Abilities
+    TBD
+
+Feats
+    TBD
+'''
+class Dwarf(Race):
+    def apply_stat_bonus(self, character):
+        character.str_score += 1
+        character.con_score += 1
